9c6b71c72379503189b65a26f46ccb45b2a5a956
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-07-10 17:18:54 +0200

    testing deploy on branch xcube98_dzelge_conda_package_deploy

--- upload_to_anaconda_cloud.sh
+++ upload_to_anaconda_cloud.sh
@@ -7,32 +7,22 @@ echo "event:   $TRAVIS_EVENT_TYPE"
 echo "package: $1"
 echo
 
-if [[ "$TRAVIS_BRANCH" = "xcube98_dzelge_conda_package_deploy" ]]; then
-    if [[ "$TRAVIS_EVENT_TYPE" = "api" || "$TRAVIS_EVENT_TYPE" = "push" ]]; then
-        CONDA_PACKAGES=( $(conda build -c conda-forge -c defaults xcube --output) )
-        echo "Start Processing"
+CONDA_PACKAGES=( $(conda build -c conda-forge -c defaults xcube --output) )
+echo "Start Processing"
 
-        echo "Converting package to other platforms"
-        platforms=( osx-64 win-64 )
+echo "Converting package to other platforms"
+platforms=( osx-64 win-64 )
 
-        for package in "${CONDA_PACKAGES[@]}"
-        do
-            for platform in "${platforms[@]}"
-            do
-                conda convert --platform ${platform} ${package}  -o $HOME/miniconda/conda-bld/
-            done
-        done
+for package in "${CONDA_PACKAGES[@]}"
+do
+    for platform in "${platforms[@]}"
+    do
+        conda convert --platform ${platform} ${package}  -o $HOME/miniconda/conda-bld/
+    done
+done
 
-        echo "Uploading packages to conda"
-        find $HOME/miniconda/conda-bld/ -name *.tar.bz2 | while read file
-        do
-            anaconda -v -t ${anaconda_token} upload ${file} -u bc-dev --force;
-        done
-    else
-        echo "NOT uploading to Anaconda Cloud, because this is the wrong event type";
-        echo "event $TRAVIS_EVENT_TYPE"
-    fi
-else
-    echo "NOT uploading to Anaconda Cloud, because this is the wrong branch";
-    echo "branch $TRAVIS_BRANCH";
-fi
+echo "Uploading packages to conda"
+find $HOME/miniconda/conda-bld/ -name *.tar.bz2 | while read file
+do
+    anaconda -v -t ${anaconda_token} upload ${file} -u bc-dev --force;
+done
