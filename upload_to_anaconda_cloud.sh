6ba48b76c4bfd2c046d4a580cc5cd63f43936a69
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-07-10 16:42:53 +0200

    Changed CONDA_PACKAGES to array

--- upload_to_anaconda_cloud.sh
+++ upload_to_anaconda_cloud.sh
@@ -14,7 +14,7 @@ anaconda_token=bc-abede329-dc1c-4db5-aa62-b1dd9caeee23
 if [[ "$TRAVIS_BRANCH" = "master" ]]; then
     if [[ "$TRAVIS_EVENT_TYPE" = "api" || "$TRAVIS_EVENT_TYPE" = "push" ]]; then
         CONDA_PACKAGES=( $(conda build -c conda-forge -c defaults xcube --output) )
-        echo "Processing ${CONDA_PACKAGES}"
+        echo "Start Processing"
 
         echo "Converting package to other platforms"
         platforms=( osx-64 win-64 )
