329e59b1b452521af1afd4a583616048519ef694
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-07-11 13:06:11 +0200

    upload_conda was attempting to upload using variable file which is empty

--- upload_to_anaconda_cloud.sh
+++ upload_to_anaconda_cloud.sh
@@ -7,7 +7,7 @@ echo "event:   $TRAVIS_EVENT_TYPE"
 echo "package: $1"
 echo
 
-CONDA_PACKAGE=( $(conda build -c conda-forge -c defaults xcube --output) )
+CONDA_PACKAGE=$(conda build -c conda-forge -c defaults xcube --output)
 echo "Start Processing"
 
 #echo "Converting package to other platforms"
@@ -24,5 +24,6 @@ echo "Start Processing"
 echo "Uploading packages to conda"
 find $HOME/miniconda/conda-bld/ -name *.tar.bz2 | while read file
 do
+    echo anaconda -v -t ${anaconda_token} upload ${CONDA_PACKAGE} -u bc-dev --force;
     anaconda -v -t ${anaconda_token} upload ${CONDA_PACKAGE} -u bc-dev --force;
 done
