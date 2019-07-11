98a26f67d59e46c7d0bcf6ea3d4bc8757c8618e2
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-07-11 15:07:26 +0200

    Attempting to install patched anaconda client version

--- upload_to_anaconda_cloud.sh
+++ upload_to_anaconda_cloud.sh
@@ -7,19 +7,19 @@ echo "event:   $TRAVIS_EVENT_TYPE"
 echo "package: $1"
 echo
 
-#CONDA_PACKAGES=$(conda build -c conda-forge -c defaults xcube --output)
+CONDA_PACKAGES=( $(conda build -c conda-forge -c defaults xcube --output) )
 echo "Start Processing"
 
-#echo "Converting package to other platforms"
-#platforms=( osx-64 win-64 )
-#
-#for package in "${CONDA_PACKAGES[@]}"
-#do
-#    for platform in "${platforms[@]}"
-#    do
-#        conda convert --platform ${platform} ${package}  -o $HOME/miniconda/conda-bld/
-#    done
-#done
+echo "Converting package to other platforms"
+platforms=( osx-64 win-64 )
+
+for package in "${CONDA_PACKAGES[@]}"
+do
+    for platform in "${platforms[@]}"
+    do
+        conda convert --platform ${platform} ${package}  -o $HOME/miniconda/conda-bld/
+    done
+done
 
 echo "Uploading packages to conda"
 
