70202d996b990249225f586959200148bd1b5551
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-07-11 10:56:21 +0200

    Removed the convert section. Does not work on travis

--- upload_to_anaconda_cloud.sh
+++ upload_to_anaconda_cloud.sh
@@ -7,22 +7,24 @@ echo "event:   $TRAVIS_EVENT_TYPE"
 echo "package: $1"
 echo
 
-CONDA_PACKAGES=( $(conda build -c conda-forge -c defaults xcube --output) )
+CONDA_PACKAGE=( $(conda build -c conda-forge -c defaults xcube --output) )
 echo "Start Processing"
 
-echo "Converting package to other platforms"
-platforms=( osx-64 win-64 )
+anaconda -v -t ${anaconda_token} upload ${CONDA_PACKAGE} -u bc-dev --force;
 
-for package in "${CONDA_PACKAGES[@]}"
-do
-    for platform in "${platforms[@]}"
-    do
-        conda convert --platform ${platform} ${package}  -o $HOME/miniconda/conda-bld/
-    done
-done
+#echo "Converting package to other platforms"
+#platforms=( osx-64 win-64 )
+#
+#for package in "${CONDA_PACKAGES[@]}"
+#do
+#    for platform in "${platforms[@]}"
+#    do
+#        conda convert --platform ${platform} ${package}  -o $HOME/miniconda/conda-bld/
+#    done
+#done
 
-echo "Uploading packages to conda"
-find $HOME/miniconda/conda-bld/ -name *.tar.bz2 | while read file
-do
-    anaconda -v -t ${anaconda_token} upload ${file} -u bc-dev --force;
-done
+#echo "Uploading packages to conda"
+#find $HOME/miniconda/conda-bld/ -name *.tar.bz2 | while read file
+#do
+#    anaconda -v -t ${anaconda_token} upload ${file} -u bc-dev --force;
+#done
